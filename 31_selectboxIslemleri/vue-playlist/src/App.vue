fa2a2e859433f11a820910a05f578928a927dae1
sekune <ahlatlipost@gmail.com>
2024-01-16 17:00:27 +0300

    added 32

---
+++ 31_selectboxIslemleri/vue-playlist/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <blog-app />
+</template>
+
+<script>
+// import formOne from "./components/form-one.vue";
+import blogApp from "./components/blog-app.vue";
+
+export default {
+  components: {
+    "blog-app": blogApp,
+  },
+  data() {
+    return { component: "form-two" };
+  },
+};
+</script>
+<style scoped></style>
---
+++ 31_selectboxIslemleri/vue-playlist/src/components/blog-app.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="add-blog">
+    <h2>Yeni Blog Post Ekleme</h2>
+    <form action="">
+      <label for="title">Blog Başlığı</label>
+      <!-- <input type="text" v-model.number="blog.title" required /> -->
+      <!-- <input type="text" v-model.trim="blog.title" required /> -->
+
+      <input type="text" v-model.lazy="blog.title" required />
+      <label for="content">Blog İçeriği</label>
+      <input type="text" v-model.lazy="blog.content" required />
+
+      <div id="checkboxes">
+        <label>Çizgi Roman</label>
+        <input type="checkbox" value="Çizgi Roman" v-model="blog.categories" />
+        <label>Hikaye</label>
+        <input type="checkbox" value="Hikaye" v-model="blog.categories" />
+        <label>Masal</label>
+        <input type="checkbox" value="Masal" v-model="blog.categories" />
+        <label>Şiir</label>
+        <input type="checkbox" value="Şiir" v-model="blog.categories" />
+      </div>
+
+      <label for="author">Yazar:</label>
+      <!-- seçilen değişken v-model="blog.author ile author'un içerisine kaydediliyor. -->
+      <select v-model="blog.author">
+        <option value="">Lütfen Yazar Seçiniz</option>
+        <option v-for="author in authors" v-bind:value="author">
+          {{ author }}
+        </option>
+      </select>
+      <!-- <button v-on:click.prevent="post">Blog'u Kaydet</button> -->
+    </form>
+
+    <div id="preview">
+      <h3>Blog Ön İzleme</h3>
+      <p>Blog Başlığı: {{ blog.title }}</p>
+      <p>Blog İçeriği: {{ blog.content }}</p>
+      <br />
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
+      <p>Yazar: {{ blog.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blog: {
+        title: "",
+        content: "",
+        categories: [],
+        author: "",
+      },
+      authors: ["Ali", "Kübra", "Semra", "Esra", "Nevra"],
+    };
+  },
+  methods: {
+    post: function () {},
+  },
+};
+</script>
+
+<style scoped>
+#add-blog * {
+  box-sizing: border-box;
+}
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
+}
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+h3 {
+  margin-top: 10px;
+}
+
+#checkboxes input {
+  display: inline-block;
+  margin-right: 15px;
+}
+#checkboxes label {
+  display: inline-block;
+}
+</style>
