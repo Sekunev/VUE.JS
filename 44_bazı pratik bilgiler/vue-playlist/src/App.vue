3d22ea54db6ec7b8bf6cb0b641730e8ca29d0602
sekune <ahlatlipost@gmail.com>
2024-01-17 12:12:21 +0300

    44 added

---
+++ 44_bazı pratik bilgiler/vue-playlist/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p><deneme></deneme></p>
+    <!--! "v-bind" yerine -->
+    ":" -->
+
+    <a :target="target" :href="link">Webtangular'a Git</a>
+    <!--! "v-on" yerine -->
+    "@"-->
+
+    <p><button @click="guncelle">Link'i Güncelle</button></p>
+  </div>
+</template>
+
+<script>
+import deneme from "./components/deneme.vue";
+
+export default {
+  components: {
+    deneme: deneme,
+  },
+  data() {
+    return {
+      link: "",
+      target: "",
+    };
+  },
+  methods: {
+    guncelle() {
+      this.link = "https://webtangular.com";
+      this.target = "_blank";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 44_bazı pratik bilgiler/vue-playlist/src/components/deneme.vue
@@ -0,0 +1,8 @@
+<template>
+  <!--! "../assets/img/image.gif" yerine "@/assets/img/image.gif" -->
+  <img
+    src="@/assets/img/image.gif"
+    alt="Deneme Görseli"
+    title="Deneme Görseli"
+  />
+</template>
